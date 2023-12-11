a6703d85fea1f52a6b269d12957b12d4aa594455
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-12-12 00:04:51 +0530

    Google maps

--- toll-calculator/src/Components/About.jsx
+++ toll-calculator/src/Components/About.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export const About = () => {
+const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h2 className="text-primary">About MapUp</h2>
+          <p className="text-muted">
+            At MapUp, we are passionate about transforming mapping into a practical decision-making tool for commuters, commercial transporters, city planners, and mobility providers.
+          </p>
+
+          <p className="text-muted">
+            We are building a unified routing intelligence and payment platform by integrating disparate & dynamic data sources like curb data, weather, traffic, tolls, parking, and payments. As the world transitions into autonomous and on-demand TaaS, multi-vector routing will become the preferred paradigm. We are well placed to ride this shift in mobility.
+          </p>
+
+          <p className="text-muted">
+            TollGuru, our marquee product, is the world's largest toll intelligence platform with complete coverage for toll roads in 80+ countries across North America, South America, Europe, parts of Asia, and Australia. Our robust technology platform delivers reliability, efficiency, and a seamless experience for rideshares, OEMs, TMS providers, fleets, and truckers.
+          </p>
+
+          <p className="text-muted">
+            We are growing fast with a network of 3 million+ users and ~1000 last-mile delivery, rideshare, logistics companies, and owner operators relying on our route intelligence platform.
+          </p>
+
+          <p className="text-muted">
+            We are looking for people from all backgrounds who want to make an impact on the millions of drivers who keep our world moving. We use big data and GIS tools combined with machine learning and AI techniques to change the way people get around. We’re creative problem solvers spread across the globe. We learn fast, think big and support each other’s growth and development.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default About;
--- toll-calculator/src/Components/RouteVisu.jsx
+++ toll-calculator/src/Components/RouteVisu.jsx
@@ -1,97 +1,59 @@
 import React, { useState } from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
-import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, DirectionsRenderer } from '@react-google-maps/api';
 
 const RouteVisu = () => {
+  const [directions, setDirections] = useState(null);
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
-  const [directions, setDirections] = useState(null);
 
   const handleCalculateRoute = () => {
+    if (!origin || !destination) {
+      console.error('Please enter both origin and destination.');
+      return;
+    }
+
     const directionsService = new window.google.maps.DirectionsService();
 
     directionsService.route(
       {
-        origin,
-        destination,
-        travelMode: window.google.maps.TravelMode.DRIVING,
+        origin: origin,
+        destination: destination,
+        travelMode: 'DRIVING',
       },
-      (result, status) => {
-        if (status === window.google.maps.DirectionsStatus.OK) {
-          setDirections(result);
+      (response, status) => {
+        if (status === 'OK') {
+          setDirections(response);
         } else {
-          console.error('Error fetching directions:', result);
+          console.error('Error fetching directions:', status);
         }
       }
     );
   };
 
   return (
-    // <div
-    //   style={{
-    //     position: 'relative',
-    //     overflow: 'hidden',
-    //     width: '100vw',
-    //     height: '100vh',
-    //   }}
-    // >
-    //   <video
-    //     autoPlay
-    //     muted
-    //     loop
-    //     playsInline
-    //     poster={process.env.PUBLIC_URL + '/video-thumbnail.jpg'}
-    //     style={{
-    //       width: '100%',
-    //       height: '100%',
-    //       objectFit: 'cover',
-    //     }}
-    //   >
-    //     <source
-    //       src={process.env.PUBLIC_URL + '/Assets/bg.mp4'}
-    //       type="video/mp4"
-    //     />
-    //     Your browser does not support the video tag.
-    //  </video>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: 1, padding: '15px' }}>
+        <div>
+          <label>Origin: </label>
+          <input type="text" value={origin} onChange={(e) => setOrigin(e.target.value)} />
+        </div>
+        <div>
+          <label>Destination: </label>
+          <input type="text" value={destination} onChange={(e) => setDestination(e.target.value)} />
+        </div>
+        <button onClick={() => handleCalculateRoute()}>Calculate Route</button>
+      </div>
 
-      <Container style={{ marginTop: '100px', marginBottom: '285px', position: 'relative', zIndex: 1 }}>
-        <Form>
-          <Form.Group controlId="formOrigin">
-            <Form.Label>Origin</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter origin"
-              value={origin}
-              onChange={(e) => setOrigin(e.target.value)}
-            />
-          </Form.Group>
-
-          <Form.Group controlId="formDestination">
-            <Form.Label>Destination</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter destination"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-            />
-          </Form.Group>
-
-          <Button variant="primary" onClick={handleCalculateRoute}>
-            Calculate Route
-          </Button>
-        </Form>
-
-        <LoadScript googleMapsApiKey="AIzaSyAHDGBteXpbAZU97q19-XXqMVRiNx9HWEM">
-          <GoogleMap
-            mapContainerStyle={{ width: '100%', height: '100%' }}
-            center={{ lat: 40.7128, lng: -74.0060 }}
-            zoom={10}
-          >
-            {directions && <DirectionsRenderer directions={directions} />}
-          </GoogleMap>
-        </LoadScript>
-      </Container>
-   // </div>
+      <LoadScript googleMapsApiKey="AIzaSyAHDGBteXpbAZU97q19-XXqMVRiNx9HWEM">
+        <GoogleMap
+          center={{ lat: 28.7041, lng: 77.1025 }}
+          zoom={10}
+          mapContainerStyle={{ flex: 1, height: '100%' }}
+        >
+          {directions && <DirectionsRenderer directions={directions} />}
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 };
 
